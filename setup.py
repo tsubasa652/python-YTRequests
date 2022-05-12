93e58e88a3e8a70d5f593c8da962e27443c77b55
tsubasa652 <tsuta906@gmail.com>
2022-05-13 02:03:41 +0900

    setup.pyの修正

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='YTRequest',
+    name='YTRequests',
     author="tsubasa652",
+    author_email="mail@tsubasa.ml",
     version='1.0.0',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=["requests"]
 )
